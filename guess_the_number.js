d560886967ba8c95d4cdcf5b5fb767d646471d04
Caleb Thill <calebnthill@gmail.com>
2014-06-18 19:51:03 -0400

    LA js challenge

---
+++ guess_the_number.js
@@ -0,0 +1,18 @@
+var num = Math.floor((Math.random() * 100) + 1);
+
+var userName = prompt("What is your freaking name?");
+var guess = prompt("whats your guess?????")
+
+
+for(num; guess != num; guess = prompt("guess")){
+    guess;
+  }
+
+alert("You guessed it!")
+
+
+
+
+
+
+
